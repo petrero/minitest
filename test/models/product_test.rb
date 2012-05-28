f845f0b5d73b06b53391e11cb1285bdb96192476
Petre Diaconescu <petrero@gmail.com>
2012-05-28 17:17:34 +0300

    changing test to look closer to rspec DLS

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,8 @@
 require "minitest_helper"
 
-class ProductTest < MiniTest::Unit::TestCase
-  def test_to_param
+describe Product do
+  it "includes name in to_param" do
     product = Product.create!(name: "Hello World!")  
-    assert_equal "#{product.id}-hello-world", product.to_param
+    product.to_param.must_equal "#{product.id}-hello-world"
   end
 end
